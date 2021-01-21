986d5131bac291e141a2512423aadafbf8a5cdad
pyrata <jsoa6@hotmail.com>
2021-01-21 18:29:40 -0500

    final

---
+++ graphql/resolvers.js
@@ -0,0 +1,10 @@
+import { getMovies, getMovie, getSuggestions } from './db';
+const resolvers = {
+	Query: {
+		movies: (_, { rating, limit }) => getMovies(limit, rating),
+		movie: (_, { id }) => getMovie(id),
+		suggestions: (_, { id }) => getSuggestions(id),
+	},
+};
+
+export default resolvers;
